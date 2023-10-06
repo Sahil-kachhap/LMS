dc03f846240b501ee734a0541829c3130969f0b5
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-07 01:13:06 +0530

    feat: Login and Logout functionality

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { activateUser, registerUser } from "../controller/user.controller";
+import { activateUser, loginUser, logoutUser, registerUser } from "../controller/user.controller";
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/activate', activateUser);
+userRouter.post('/login', loginUser);
+userRouter.post('/logout', logoutUser);
 
 export default userRouter;
