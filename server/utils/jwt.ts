e2538e2e5ddeaab9dd1eea9294bf1865df1fde82
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-07 14:22:45 +0530

    chore: make token options code reusable from anywhere

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -12,30 +12,30 @@ interface ITokenOptions {
   secure?: boolean;
 }
 
+export const accessTokenExpire = parseInt(process.env.ACCESS_EXPIRE || '300', 10);
+export const refreshTokenExpire = parseInt(process.env.REFRESH_EXPIRE || '1200', 10);
+
+// cookie options
+export const accessTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  sameSite: 'lax',
+  httpOnly: true
+}
+
+export const refreshTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+  sameSite: 'lax',
+  httpOnly: true
+}
+
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   // store user session in redis once they log in
   redis.set(user._id, JSON.stringify(user) as any);
 
   const accessToken = user.SignAccessToken();
   const refreshToken = user.SignRefreshToken();
-
-  const accessTokenExpire = parseInt(process.env.ACCESS_EXPIRE || '300', 10);
-  const refreshTokenExpire = parseInt(process.env.REFRESH_EXPIRE || '1200', 10);
-
-  // cookie options
-  const accessTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 1000),
-    maxAge: accessTokenExpire * 1000,
-    sameSite: 'lax',
-    httpOnly: true
-  }
-
-  const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 1000),
-    maxAge: refreshTokenExpire * 1000,
-    sameSite: 'lax',
-    httpOnly: true
-  }
 
   if (process.env.NODE_ENV === "production") {
     accessTokenOptions.secure = true;
