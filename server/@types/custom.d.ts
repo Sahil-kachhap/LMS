11eb26a2fb3bce23726f9ef70749d198e0acf87a
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-07 11:17:06 +0530

    chore: added user parameter to request object in express

---
+++ server/@types/custom.d.ts
@@ -0,0 +1,10 @@
+import { IUser } from "../model/user.model";
+import { Request } from "express";
+
+declare global {
+    namespace Express{
+        interface Request{
+            user?:IUser
+        }
+    }
+}
