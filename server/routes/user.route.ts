9ef8b2073e876ac260ab4d424eb0df5ff2367981
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-07 11:26:59 +0530

    feat: added logout endpoint

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { activateUser, loginUser, logoutUser, registerUser } from "../controller/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
+// user registration api endpoint
 userRouter.post('/register', registerUser);
+
+// user account activation api endpoint
 userRouter.post('/activate', activateUser);
+
+// user login api endpoint
 userRouter.post('/login', loginUser);
-userRouter.post('/logout', logoutUser);
+
+// logout user endpoint
+userRouter.get('/logout', isAuthenticated, authorizeRoles("admin"), logoutUser);
 
 export default userRouter;
