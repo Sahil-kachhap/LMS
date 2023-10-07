aa6e7c9e26a1ded4d6fa8d8b64c0663181d3c356
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-07 20:33:16 +0530

    chore: configured cloudinary connection

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,15 @@
 import { app } from "./app"
 import { connectDB } from "./utils/db";
+import {v2 as cloudinary} from "cloudinary";
 require("dotenv").config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET,
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`);
     connectDB();
-})
+});
