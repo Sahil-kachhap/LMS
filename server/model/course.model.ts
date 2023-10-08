4af871aefd651f1fc88c570b144529695b15b1d9
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-08 12:43:30 +0530

    chore: videoUrl contains thumbnail so no need to explicitly declare thumbnail property

--- server/model/course.model.ts
+++ server/model/course.model.ts
@@ -70,7 +70,6 @@ const commentSchema = new Schema<IComment>({
 
 const courseDataSchema = new Schema<ICourseData>({
    videoUrl: String,
-   videoThumbnail: Object,
    title: String,
    videoSection: String,
    description: String,
@@ -100,10 +99,8 @@ const courseSchema = new Schema<ICourse>({
     thumbnail: {
         public_id: {
             type: String,
-            required: true,
         },
         url: {
-            required: true,
             type: String,
         },
     },
