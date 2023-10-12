74ad6a0f3df9d8ff88c9a0c2d9bdedb2957d4808
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2023-10-12 14:57:20 +0530

    fix: res is not a function

--- server/services/order.service.ts
+++ server/services/order.service.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response } from "express";
 import { catchAsyncError } from "../middleware/catch_async_error";
 import OrderModel from "../model/order.model";
 
-export const newOrder = catchAsyncError(async (data: any, next: NextFunction, res: Response) => {
+export const newOrder = catchAsyncError(async (data: any, res: Response, next: NextFunction) => {
     const order = await OrderModel.create(data);
 
     res.status(201).json({
